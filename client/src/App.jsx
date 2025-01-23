3717e225f49c78c0f1ab0d8730352ecfcac274e5
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-23 18:37:34 +0200

    added home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Home from "./components/home/Home";
 
 function App() {
   return (
     <div id="box">
       <Header />
+      <Home />
     </div>
   );
 }
