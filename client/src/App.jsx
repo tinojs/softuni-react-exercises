1f055af58e551aa2080fc856cd0c0bb60369eef8
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-24 13:45:54 +0200

    add react router library
    add game list page
    added nav links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
+import Gamelist from "./components/game-list/GameList";
 
 function App() {
   return (
     <div id="box">
       <Header />
-      <Home />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/games" element={<Gamelist />} />
+      </Routes>
     </div>
   );
 }
