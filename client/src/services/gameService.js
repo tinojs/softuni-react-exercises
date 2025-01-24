c1009315901ba8bed22b329063fcab10d1c44c81
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-24 17:21:47 +0200

    add get all games service

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,7 +1,17 @@
-const baseUrl = 'http://localhost:3030/jsonstore'
+import { request } from "../lib/request";
+const baseUrl = 'http://localhost:3030/jsonstore/games'
+
+export const getAll = async () => {
+    const result = await request('GET', baseUrl);
+
+    console.log(result);
+
+    return Object.values(result);
+
+};
 
 export const create = async (gameData) => {
-    const response = await fetch(`${baseUrl}/games`, {
+    const response = await fetch(baseUrl, {
         method: 'POST',
         headers: {
             'content-type': 'application/json'
@@ -12,8 +22,9 @@ export const create = async (gameData) => {
     const result = await response.json();
 
     return result;
-}
+};
+
 
 //we take the data from the form(gameData) and we make a request to post to the server. body - takes the game data object and converts it into string.
 
-//the idea of this service is to group the resources so we can make all CRUD requests.
+//the idea of this service is to group the resources so we can make all CRUD requests. 
