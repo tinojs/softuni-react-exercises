f9ddb0a45687dd76f1667c54206522182e6272d7
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-24 17:46:18 +0200

    show dynamic game list

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -2,12 +2,10 @@ import { request } from "../lib/request";
 const baseUrl = 'http://localhost:3030/jsonstore/games'
 
 export const getAll = async () => {
+
     const result = await request('GET', baseUrl);
 
-    console.log(result);
-
     return Object.values(result);
-
 };
 
 export const create = async (gameData) => {
