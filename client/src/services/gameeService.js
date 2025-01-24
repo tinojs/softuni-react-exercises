c1dabfdca98de4426064cea5c89d08ecacd91aba
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-24 15:56:04 +0200

    created game service

---
+++ client/src/services/gameeService.js
@@ -0,0 +1,19 @@
+const baseUrl = 'http://localhost:3030/jsonstore'
+
+export const create = async (gameData) => {
+    const response = await fetch(`${baseUrl}/games`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(gameData)
+    });
+
+    const result = await response.json();
+
+    return result;
+}
+
+//we take the data from the form(gameData) and we make a request to post to the server. body - takes the game data object and converts it into string.
+
+//the idea of this service is to group the resources so we can make all CRUD requests.
