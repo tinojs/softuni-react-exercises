3495af304a701cf60d044bfe11bdae449a4e2698
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-29 11:21:49 +0200

    add login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import AuthContext from "./contexts/authContext";
+import * as authService from "./services/authService";
+
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
 import Gamelist from "./components/game-list/GameList";
@@ -6,14 +11,18 @@ import GameCreate from "./components/game-create/GameCreate";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
 import GameDetails from "./components/game-details/GameDetails";
-import { useState } from "react";
-import AuthContext from "./contexts/authContext";
 
 function App() {
+  const navigate = useNavigate();
+
   const [auth, setAuth] = useState({});
 
-  const loginSubmitHandler = (values) => {
-    console.log(values);
+  const loginSubmitHandler = async (values) => {
+    const result = await authService.login(values.email, values.password);
+
+    setAuth(result);
+
+    navigate("/");
   };
 
   return (
