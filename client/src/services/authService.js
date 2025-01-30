ddd4b6c5ff3d13c3997cdb3dc0dacc6604bf74f6
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-30 15:47:45 +0200

    ADD USER REGISTER

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -7,3 +7,5 @@ export const login = async (email, password) => {
 
     return result;
 };
+
+export const register = async (email, password) => request.post(`${baseUrl}/register`, { email, password });
