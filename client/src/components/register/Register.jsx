2394b3ffba5281e7c230827598c09593240cd3bb
tinojs <135985164+tinojs@users.noreply.github.com>
2025-01-30 15:39:54 +0200

    add register

--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import useForm from "../../hooks/useForm";
+import AuthContext from "../../contexts/authContext";
+
+const RegisterFormKeys = {
+  Email: "email",
+  Password: "password",
+  ConfirmPassword: "confirm-password",
+};
 export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    [RegisterFormKeys.Email]: "",
+    [RegisterFormKeys.Password]: "",
+    [RegisterFormKeys.ConfirmPassword]: "",
+  });
+
   return (
     <section id="register-page" className="content auth">
-      <form id="register">
+      <form id="register" onSubmit={onSubmit}>
         <div className="container">
           <div className="brand-logo" />
           <h1>Register</h1>
@@ -11,14 +27,24 @@ export default function Register() {
             id="email"
             name="email"
             placeholder="maria@email.com"
+            onChange={onChange}
+            values={values[RegisterFormKeys.Email]}
           />
           <label htmlFor="pass">Password:</label>
-          <input type="password" name="password" id="register-password" />
+          <input
+            type="password"
+            name="password"
+            id="register-password"
+            onChange={onChange}
+            values={values[RegisterFormKeys.Password]}
+          />
           <label htmlFor="con-pass">Confirm Password:</label>
           <input
             type="password"
             name="confirm-password"
             id="confirm-password"
+            onChange={onChange}
+            values={values[RegisterFormKeys.ConfirmPassword]}
           />
           <input className="btn submit" type="submit" defaultValue="Register" />
           <p className="field">
